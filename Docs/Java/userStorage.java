e8440dfb003b32b8a1666cb6d7709d6f5c9bf9a8
Katherine Blizard <kblizard@calpoly.edu>
2009-03-05 23:48:36 +0000

    updating Teacher java class

---
+++ Docs/Java/userStorage.java
@@ -0,0 +1,9 @@
+
+/** UserStorage is a storage database that stores users not associated with any Course.
+ *
+ * @author blugoo
+ * @version 0.42
+ */
+public class UserStorage implements Storage 
+{
+}
