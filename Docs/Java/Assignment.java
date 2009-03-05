853c57552dbc8f3de1990c9309b9feff12f10a34
Nat Welch <nat@natwelch.com>
2009-03-05 21:03:04 +0000

    Yay arch work.

--- Docs/Java/Assignment.java
+++ Docs/Java/Assignment.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.jang.*;
 
 /**
  * This class keeps information about Assignments
@@ -7,16 +9,82 @@
  */
 public class Assignment
 {
-    /** The due date of the Assignment */
-    private String dueDate;
+    /** 
+	 * The due date of the Assignment 
+	 */
+    private Date dueDate;
     
-    /** The name of the Assignment */
+    /**
+	 * The name of the Assignment 
+	 */
     private String name;
     
-    /** The maximum possible score of the Assignment */
-    private int assignmentTotal;
+    /**
+	 * The maximum possible score of the Assignment 
+	 */
+    private Integer total;
     
-    /** The type of the Assignment; can be Test, Quiz, Homework, Participation, or other */
-    private short type;
+    /**
+	 * The type of the Assignment; can be Test, Quiz, Homework, Participation, or other
+	 */
+    private String type;
+
+	/**
+	 * The average for all graded assignments for this assignment
+	 */
+	private Float average;
+	
+	/**
+	 * The maximum for all graded assignments for this assignment
+	 */
+	private Float max;
+
+	/**
+	 * The minimum for all graded assignments for this assignment
+	 */
+	private Float min;
+
+	public Date getDueDate()
+	{
+		return new Date();
+	}
+
+	public String getName()
+	{
+		return new String();
+	}
+
+	public Integer getTotal()
+	{
+		return 0;
+	}
+
+	public String getType()
+	{
+		return new String();
+	}
+
+	/*
+	public AssignmentMetric getMetrics()
+	{
+
+	}
+*/
+
+	public Float getAvg()
+	{
+		return this.average;
+	}
+
+	public Float getMax()
+	{
+		return this.max;
+	}
+
+	public Float getMin()
+	{
+		return this.min
+	}
+
 }
 
--- Docs/Java/Email.java
+++ Docs/Java/Email.java
@@ -2,7 +2,9 @@ import java.lang.*;
 import java.util.*;
 
 /**
- * The class stores information about Emails.
+ * The class stores information about Emails. These emails will not be stored 
+ * in the database. This is purely a way to create a wrapper for validation and 
+ * sending of emails.
  *
  * @author bluGoo
  * @version 0.42
@@ -16,7 +18,7 @@ public class Email
     private String emailAttachment;
     
     /** An array of email addresses to which the Email will be sent */
-    private ArrayList<String> emailRecipients;
+    private ArrayList<EmailAddress> emailRecipients;
     
     /** The body of the Email */
     private String emailBody;
@@ -28,4 +30,12 @@ public class Email
     public Email()
     {
     }
+
+	/**
+	 * Sends the email once it has been written.
+	 */
+	public void send()
+	{
+
+	}
 }
