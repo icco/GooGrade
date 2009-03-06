a7f40d3af0c17197698c75efa6c22a7c10456dd9
Katherine Blizard <kblizard@calpoly.edu>
2009-03-06 02:51:08 +0000

    jolly is not spelled joly

--- Docs/Java/StorageConnection.java
+++ Docs/Java/StorageConnection.java
@@ -3,12 +3,15 @@ import java.lang.*;
 
 /**
  * This is the class that deals with connecting to the storage device.
+ *
+ * @author blugoo
+ * @version 0.42
  */
 abstract class StorageConnection implements Storage
 {
 
 	/**
-	 * Connect and be joly.
+	 * Connect and be jolly.
 	 */
 	StorageConnection()
 	{
