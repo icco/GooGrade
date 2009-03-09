3adaf7de1d956b55aec407ce901e9768337bd522
Viktor Gerdin <vgerdin@calpoly.edu>
2009-03-09 01:32:46 +0000

    die bitch die!

--- Docs/Java/UserStorage.java
+++
@@ -1,9 +0,0 @@
-
-/** UserStorage is a storage database that stores users not associated with any Course.
- *
- * @author blugoo
- * @version 0.42
- */
-public class UserStorage implements Storage 
-{
-}
