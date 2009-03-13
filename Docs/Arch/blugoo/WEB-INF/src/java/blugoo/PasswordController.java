89b6cea17f8b61e8c2eaaa14430296ffc89f9776
Nat Welch <nat@natwelch.com>
2009-03-13 23:08:51 +0000

    Ok. This is getting tedious. I still haven't made any "real" progress

---
+++ Docs/Arch/blugoo/WEB-INF/src/java/blugoo/PasswordController.java
@@ -0,0 +1,27 @@
+
+package blugoo;
+
+import java.util.*;
+import java.lang.*;
+
+/**
+ * A model for passwords since they need to be validated and such.
+ *
+ * @author bluGoo
+ * @version 0.42
+ */
+public class Password 
+{
+
+	private String password;
+
+	/**
+	 * Basically a wrapper for hashing.
+	 */
+	Password(String newpass)
+	{
+
+	}
+
+
+}
