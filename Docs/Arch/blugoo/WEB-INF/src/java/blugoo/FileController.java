06d0544a064ed25f863687b4a1a05fb2f1d75a82
Paul Phu <pphu@calpoly.edu>
2009-03-16 10:04:44 +0000

    updated FileController.java, GradingRulesController.java, and 08ManageGrades.png

--- Docs/Arch/blugoo/WEB-INF/src/java/blugoo/FileController.java
+++ Docs/Arch/blugoo/WEB-INF/src/java/blugoo/FileController.java
@@ -14,20 +14,34 @@ public class FileController
 {
 	/**
 	 * Upload to system.
+	 * @param file to be uploaded 
+	 * @return true if successful, false if error.
 	 */
-	public void upload()
+	public boolean upload(File filename)
 	{
 
-
+		return false;
 	}
 
 	/**
 	 * Download a file.
+	 * @param file to be downloaded
+	 * @return true if successful, false if error.
 	 */
-	public void download()
+	public boolean download(File filename)
 	{
 
-
+		return false;
 	}
+	
+	 /**
+	 * Delete a file.
+	 * @param file to be deleted
+	 * @return true if successful, false if error.
+	 */
+	public boolean delete(File filename)
+	{
 
+		return false;
+	}
 }
