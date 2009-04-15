f07a52c493ceb050184231dcb2ab954b7a12bbb0
Viktor Gerdin <vgerdin@calpoly.edu>
2009-04-15 01:17:12 +0000

    db update

--- prototype/prototypedb.sql
+++ prototype/prototypedb.sql
@@ -21,4 +21,18 @@ CREATE TABLE does (
 	        users_id INTEGER NOT NULL
 		);	
 
+INSERT INTO users (name, email) VALUES ("Nat Welch", "nwelch@calpoly.edu");
+INSERT INTO users (name, email) VALUES ("Viktor Gerdin", "vgerdin@calpoly.edu");
+INSERT INTO users (name, email) VALUES ("Matt Duder", "mduder@calpoly.edu");
+INSERT INTO users (name, email) VALUES ("Kat Blizard", "kblizard@calpoly.edu");
+INSERT INTO users (name, email) VALUES ("Paul Phu", "pphu@calpoly.edu");
 
+INSERT INTO tasks (startDate, dueDate, what) VALUES (date('now'), NULL, "Eat shit and compile");
+INSERT INTO tasks (startDate, dueDate, what) VALUES (date('now'), date('now','+1 month'), "Beat up Paul");
+
+INSERT INTO does (tasks_id, users_id) VALUES (5,1);
+INSERT INTO does (tasks_id, users_id) VALUES (1,2);
+INSERT INTO does (tasks_id, users_id) VALUES (2,2);
+INSERT INTO does (tasks_id, users_id) VALUES (3,2);
+INSERT INTO does (tasks_id, users_id) VALUES (4,2);
+INSERT INTO does (tasks_id, users_id) VALUES (5,2);
