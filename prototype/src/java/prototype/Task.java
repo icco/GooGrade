f0fb44dc11251da22aa576c2215509eb13fdca6d
Viktor Gerdin <vgerdin@calpoly.edu>
2009-04-15 02:15:01 +0000

    played around with sql stuff, where can I find the javax.persistance package?

--- prototype/src/java/prototype/Task.java
+++ prototype/src/java/prototype/Task.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import java.sql.*;
+
 /**
  *
  * @author nwelch
@@ -21,7 +23,31 @@ public class Task implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private Long startDate;
+    private Long dueDate;
+    private String what;
 
+    Connection conn = DriverManager.getConnection("jdbc:sqlite://home/vgerdin/db/ToDo.db");
+    Statement stat = conn.createStatement();
+    PreparedStatement prep = conn.prepareStatement("SELECT id, startDate, dueDate, what FROM tasks WHERE id = ?;");
+    ResultSet rs;
+    
+    public Task (Long id){
+        prep.setLong(1,id);
+        
+        conn.setAutoCommit(false);
+        rs = prep.executeQuery();
+        conn.setAutoCommit(true);
+        
+        if(rs.next()){
+            this.id = id;
+            this.startDate = rs.getLong("startDate");
+            this.dueDate = rs.getLong("dueDate");
+            this.what = rs.getString("what");
+        }
+        
+    }
+    
     public Long getId() {
         return id;
     }
@@ -53,6 +79,30 @@ public class Task implements Serializable {
     @Override
     public String toString() {
         return "prototype.Task[id=" + id + "]";
+    }
+
+    public Long getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Long startDate) {
+        this.startDate = startDate;
+    }
+
+    public Long getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Long dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what;
     }
 
 }
