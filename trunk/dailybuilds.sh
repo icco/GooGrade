21403cfc9930a7e74a87ba5dac19916f149f0e02
Nat Welch <nat@natwelch.com>
2009-04-22 05:45:39 +0000

    Alright, daily builds are pretty awesome.

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -4,9 +4,14 @@
 # Variables
 SUBJECT="Daily Build Report for `date +%D`"
 EMAIL="blugoo@lists.csc.calpoly.edu"
-MESSAGE_FILE="output.`date +%F`"
+MESSAGE_FILE="/var/www/html/logs/output.`date +%F`.log"
 
-make -C /var/www/html/svn/trunk/ publish &> $MESSAGE_FILE
+#make -C /var/www/html/svn/trunk/ publish &> $MESSAGE_FILE
 
+cd /var/www/html/svn/trunk/goograde/;
+ant clean &> /dev/null
+ant -logfile $MESSAGE_FILE &> /dev/null
+
+#Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
 
