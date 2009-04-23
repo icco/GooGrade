b1a743aeaa35bf5d96bc7f616096dfc798f210a7
Paul Phu <pphu@calpoly.edu>
2009-04-23 07:14:48 +0000

    added Doxyfile and some settings change for ant to handle Doxygen + dailybuild.sh

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -12,6 +12,11 @@ cd /var/www/html/svn/trunk/goograde/;
 ant clean &> /dev/null
 ant -logfile $MESSAGE_FILE &> /dev/null
 
+#Setting up the webapp
+cp /var/www/html/GooGrade.war /usr/share/tomcat/webapps/
+rm -rf /user/share/tomcat/webapps/GooGrade/
+service tomcat5 restart
+
 #Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
 
