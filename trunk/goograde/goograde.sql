9017f07e0649e73875860b2a4e607170538e2e51
Nat Welch <nat@natwelch.com>
2009-04-23 04:12:23 +0000

    Adding the beginings of the sql doc.

---
+++ trunk/goograde/goograde.sql
@@ -0,0 +1,40 @@
+-- BluGoo's Awesome DataBase
+
+-- Drop Tables 
+ DROP TABLE IF EXISTS Accounts;
+ DROP TABLE IF EXISTS Courses;
+ DROP TABLE IF EXISTS Teachers;
+ DROP TABLE IF EXISTS TAs;
+ DROP TABLE IF EXISTS Students;
+ DROP TABLE IF EXISTS Files;
+ DROP TABLE IF EXISTS Assignments;
+ DROP TABLE IF EXISTS Passwords;
+ DROP TABLE IF EXISTS Permissions;
+ DROP TABLE IF EXISTS EmailAddresses;
+ DROP TABLE IF EXISTS Announcements;
+ DROP TABLE IF EXISTS GradingRules;
+ DROP TABLE IF EXISTS hasGrade;
+ DROP TABLE IF EXISTS teaches;
+ DROP TABLE IF EXISTS submitted;
+ DROP TABLE IF EXISTS hasPermission;
+ DROP TABLE IF EXISTS hasPW;
+ DROP TABLE IF EXISTS hasAddr;
+
+ Create Table Accounts (           );
+ Create Table Courses (           );
+ Create Table Teachers (           );
+ Create Table TAs (           );
+ Create Table Students (           );
+ Create Table Files (           );
+ Create Table Assignments (           );
+ Create Table Passwords (           );
+ Create Table Permissions (           );
+ Create Table EmailAddresses (           );
+ Create Table Announcements (           );
+ Create Table GradingRules (           );
+ Create Table hasGrade (           );
+ Create Table teaches (           );
+ Create Table submitted (           );
+ Create Table hasPermission (           );
+ Create Table hasPW (           );
+ Create Table hasAddr (           );
