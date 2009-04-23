3c6f9f02969ac072c4949bf6871ce09709586e1f
Nat Welch <nat@natwelch.com>
2009-04-23 23:51:50 +0000

    Whoopsie. I guess you need this class for this all to work. Not even sure if I should do this, may delete it later.

---
+++ trunk/goograde/WEB-INF/classes/GooGrade/InvalidOption.java
@@ -0,0 +1,14 @@
+package GooGrade;
+
+/**
+ * An exception to throw if something is given the wrong thing.
+ *
+ * @author nwelch
+ */
+public class InvalidOption extends java.lang.Error
+{
+    public InvalidOption()
+    {
+        super("You have chosen to perform an invalid option.");
+    }
+}
