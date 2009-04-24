3873de05a850a49d9afe8a63dc0f55efc3a70a70
Katherine Blizard <kblizard@calpoly.edu>
2009-04-24 00:23:52 +0000

    Complex Number Test is updated with new test

--- Training/g/ComplexNumberTest.java
+++ Training/g/ComplexNumberTest.java
@@ -3,30 +3,34 @@
  * and open the template in the editor.
  */
 
-
 import junit.framework.*;
+
 
 /**
  *
  * @author mduder
  */
-public class ComplexNumberTest extends TestCase {
-    
+public class ComplexNumberTest extends TestCase
+{
+
     ComplexNumber cn1, cn2;
-    
-    public ComplexNumberTest(String testName) {
+
+    public ComplexNumberTest(String testName)
+    {
         super(testName);
-    }            
+    }
 
     @Override
-    protected void setUp() throws Exception {
+    protected void setUp() throws Exception
+    {
         super.setUp();
         cn1 = new ComplexNumber(6, 8);
         cn2 = new ComplexNumber(1, 3);
     }
 
     @Override
-    protected void tearDown() throws Exception {
+    protected void tearDown() throws Exception
+    {
         super.tearDown();
     }
 
@@ -34,7 +38,8 @@ public class ComplexNumberTest extends TestCase {
      * Test of magnitude method, of class ComplexNumber.
      * @author Matthew Duder
      */
-    public void testMagnitudeDefect90() {
+    public void testMagnitudeDefect90()
+    {
         System.out.println("magnitude");
         ComplexNumber instance = cn1;
         double expResult = 10.0;
@@ -42,4 +47,12 @@ public class ComplexNumberTest extends TestCase {
         assertEquals(expResult, result);
     }
 
+    public void testGetReal()
+    {
+        System.out.println("getReal");
+        ComplexNumber instance = cn1;
+        double expResult = 6;
+        double result = instance.getReal();
+        assertEquals(expResult, result);
+    }
 }
