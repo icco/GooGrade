10b5c89c1895c75dfa0702cc566c1547d6cb4fb0
Nat Welch <nat@natwelch.com>
2009-04-26 23:56:02 +0000

    Small typo, and the cool thing is, even as root, it still records the correct commiter. Also, as a side note, Wahoo 500 commits

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -6,7 +6,7 @@ SUBJECT="Daily Build Report for `date +%D`"
 EMAIL="blugoo@lists.csc.calpoly.edu"
 MESSAGE_FILE="/var/www/html/logs/output.`date +%F`.log"
 
-./noemail-dailybuild.sh $MESSAGE_FILE
+./noemail-dailybuilds.sh $MESSAGE_FILE
 
 # Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
