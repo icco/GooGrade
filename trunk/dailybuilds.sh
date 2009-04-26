d3ce0ced2893d7c629de26eae2cb9f86df24705d
Paul Phu <pphu@calpoly.edu>
2009-04-26 08:51:05 +0000

    Bug in both daily build scripts fixed.

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -15,7 +15,7 @@ ant -f dailybuild.xml -logfile $MESSAGE_FILE &> /dev/null
 #Setting up the webapp
 cp /var/www/html/GooGrade.war /usr/share/tomcat/webapps/
 rm -rf /user/share/tomcat/webapps/GooGrade/
-service tomcat5 restart
+/sbin/service tomcat5 restart
 
 #Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
