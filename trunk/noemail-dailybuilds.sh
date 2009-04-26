5b83d96a6b483841dab6cc258885beb15335fe83
Paul Phu <pphu@calpoly.edu>
2009-04-26 09:16:35 +0000

    Added helper echos for noemail-dailybuilds.sh

--- trunk/noemail-dailybuilds.sh
+++ trunk/noemail-dailybuilds.sh
@@ -3,12 +3,13 @@
 
 
 #make -C /var/www/html/svn/trunk/ publish &> $MESSAGE_FILE
-
+echo "Running Ant"
 cd /var/www/html/svn/trunk/goograde/;
 ant clean &> /dev/null
 ant -f dailybuild.xml -logfile $MESSAGE_FILE &> /dev/null
 
 #Setting up the webapp
+echo "Deploying build"
 cp /var/www/html/wars/GooGrade.`date +%F`.war /usr/share/tomcat5/webapps/GooGrade.war
 rm -rf /user/share/tomcat5/webapps/GooGrade/
 /sbin/service tomcat5 restart
