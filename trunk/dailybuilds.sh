7ee4d7d6472f423ba412a27c4d69d420c947c45d
Nat Welch <nat@natwelch.com>
2009-04-26 23:53:27 +0000

    This is nwelch updateing the build scripts to make the code less redundent and to promote ease of use, I hope.

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -6,17 +6,8 @@ SUBJECT="Daily Build Report for `date +%D`"
 EMAIL="blugoo@lists.csc.calpoly.edu"
 MESSAGE_FILE="/var/www/html/logs/output.`date +%F`.log"
 
-#make -C /var/www/html/svn/trunk/ publish &> $MESSAGE_FILE
+./noemail-dailybuild.sh $MESSAGE_FILE
 
-cd /var/www/html/svn/trunk/goograde/;
-ant clean &> /dev/null
-ant -f dailybuild.xml -logfile $MESSAGE_FILE &> /dev/null
-
-#Setting up the webapp
-cp /var/www/html/wars/GooGrade.`date +%f`.war /usr/share/tomcat5/webapps/GooGrade.war
-rm -rf /user/share/tomcat5/webapps/GooGrade/
-/sbin/service tomcat5 restart
-
-#Mail it!!!
+# Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
 
--- trunk/noemail-dailybuilds.sh
+++ trunk/noemail-dailybuilds.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # A daily Build Script for blugoo
+# Usage ./noemail-dailybuild.sh logfile
 
+# Log info to the specified file or /tmp/build.datetime.log
+if [ $# -lt 1 ]; then
+	MESSAGE_FILE="/tmp/build.`date +%s`.log";
+else
+	MESSAGE_FILE=$1;
+fi;
 
-#make -C /var/www/html/svn/trunk/ publish &> $MESSAGE_FILE
+# Do the building
 echo "Running Ant"
 cd /var/www/html/svn/trunk/goograde/;
 ant clean &> /dev/null
 ant -f dailybuild.xml -logfile $MESSAGE_FILE &> /dev/null
 
-#Setting up the webapp
+# Deploy that which you built to the web
 echo "Deploying build"
 cp /var/www/html/wars/GooGrade.`date +%F`.war /usr/share/tomcat5/webapps/GooGrade.war
 rm -rf /user/share/tomcat5/webapps/GooGrade/
