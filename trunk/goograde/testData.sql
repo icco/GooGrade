19726d0b9e6b6bb8f6f82c62494b94100214bf4c
Nat Welch <nat@natwelch.com>
2009-04-27 04:41:02 +0000

    Updating test data

--- trunk/goograde/testData.sql
+++ trunk/goograde/testData.sql
@@ -1,3 +1,7 @@
+-- 
+-- Test Data for Development. 
+--
+
 INSERT INTO Courses (title, department, number, section, gradingRulesId) 
     VALUES("Systems Programming", "CSC", "357","01","1");
 INSERT INTO Courses (title, department, number, section, gradingRulesId) 
@@ -14,6 +18,9 @@ INSERT INTO Accounts (username, name, email, password) VALUES("pphu","Paul Phu",
 INSERT INTO Accounts (username, name, email, password) VALUES("vgerdin","Viktor Gerdin","vgerdin@calpoly.edu",123456);
 INSERT INTO Accounts (username, name, email, password) VALUES("kblizard","Katherine Blizard","kblizard@calpoly.edu",123456);
 INSERT INTO Accounts (username, name, email, password) VALUES("mduder","Matt Duder","mduder@calpoly.edu",123456);
+INSERT INTO Accounts (username, name, email, password) VALUES("kvu","Kalvin Vu","kvu@calpoly.edu",123456);
+INSERT INTO Accounts (username, name, email, password) VALUES("mquan","Michael Quan","mquan@calpoly.edu",123456);
+INSERT INTO Accounts (username, name, email, password) VALUES("hmendez","Hermyn Mendez","hmendez@calpoly.edu",123456);
 
 INSERT INTO teaches (course, teacher) VALUES (1,1);
 INSERT INTO teaches (course, teacher) VALUES (2,1);
@@ -36,3 +43,7 @@ INSERT INTO enrolled (course, student) VALUES (5, 4);
 INSERT INTO enrolled (course, student) VALUES (2, 5);
 INSERT INTO enrolled (course, student) VALUES (3, 5);
 INSERT INTO enrolled (course, student) VALUES (5, 5);
+
+-- Word to your Database. 
+-- Peace, I'm Out.
+
