b2fdc99f7b3e678d9d417e4d9ba0f373ef0ddd64
Nat Welch <nat@natwelch.com>
2009-05-02 07:20:45 +0000

    Wow, I thought I commited this a long time ago. Fixes a compile bug.

--- trunk/dailybuilds.sh
+++ trunk/dailybuilds.sh
@@ -6,7 +6,7 @@ SUBJECT="Daily Build Report for `date +%D`"
 EMAIL="blugoo@lists.csc.calpoly.edu"
 MESSAGE_FILE="/var/www/html/logs/output.`date +%F`.log"
 
-./noemail-dailybuilds.sh $MESSAGE_FILE
+/var/www/html/svn/trunk/noemail-dailybuilds.sh $MESSAGE_FILE
 
 # Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
