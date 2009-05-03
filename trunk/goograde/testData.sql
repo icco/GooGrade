11730417e9262f66828d497c1ce8351238bd1403
Viktor Gerdin <vgerdin@calpoly.edu>
2009-05-03 00:43:00 +0000

    passwords now comply to standard, #278

--- trunk/goograde/testData.sql
+++ trunk/goograde/testData.sql
@@ -14,14 +14,14 @@ INSERT INTO Courses (title, department, number, section, gradingRulesId)
     VALUES("Graduate Thesis", "CSC", "500","01","1");
 
 -- Nat's account needs to stay first, since we test that the teacher is first in the db.
-INSERT INTO Accounts (username, name, email, password) VALUES("nwelch","Nathaniel Welch","nat@natwelch.com","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("pphu","Paul Phu","pphu@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("vgerdin","Viktor Gerdin","vgerdin@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("kblizard","Katherine Blizard","kblizard@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("mduder","Matt Duder","mduder@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("kvu","Kalvin Vu","kvu@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("mquan","Michael Quan","mquan@calpoly.edu","123456");
-INSERT INTO Accounts (username, name, email, password) VALUES("hmendez","Hermyn Mendez","hmendez@calpoly.edu","123456");
+INSERT INTO Accounts (username, name, email, password) VALUES("nwelch","Nathaniel Welch","nat@natwelch.com","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("pphu","Paul Phu","pphu@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("vgerdin","Viktor Gerdin","vgerdin@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("kblizard","Katherine Blizard","kblizard@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("mduder","Matt Duder","mduder@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("kvu","Kalvin Vu","kvu@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("mquan","Michael Quan","mquan@calpoly.edu","123456a");
+INSERT INTO Accounts (username, name, email, password) VALUES("hmendez","Hermyn Mendez","hmendez@calpoly.edu","123456a");
 
 INSERT INTO teaches (course, teacher) VALUES (1,1);
 INSERT INTO teaches (course, teacher) VALUES (2,1);
