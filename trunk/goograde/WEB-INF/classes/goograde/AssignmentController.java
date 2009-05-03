90530e23528cddfa234d149aeed91c6cf3405efe
Katherine Blizard <kblizard@calpoly.edu>
2009-05-03 04:03:58 +0000

    Continuing work on Assignment Controller

--- trunk/goograde/WEB-INF/classes/goograde/AssignmentController.java
+++ trunk/goograde/WEB-INF/classes/goograde/AssignmentController.java
@@ -9,6 +9,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
 
 /**
  * Does all of the work on the Assignment, it is the controller. 
@@ -21,20 +23,28 @@ public class AssignmentController extends HttpServlet
 {
 
     @Override
+    /**
+     * doPost performs actions
+     * @param req request
+     * @param resp response
+     */
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
     {
-        String action = req.getParameter("action");
+        
+        String action = req.getParameter("action"); //the action to be done
 
+        /*Determine which action needs to be taken */
         if (action != null)
         {
             if (action.equals("delete"))
             {
-                //this.deleteTeacher(new Integer(req.getParameter("accountRef")));
+                //choose teh appropriate assignment to modify and then modify 
+                    //and then delete it
             }
             else if (action.equals("add"))
             {
                 //this.addAssignment(req.getParameter("newUserName"),
-                        }
+            }
         }
 
         try
@@ -43,16 +53,23 @@ public class AssignmentController extends HttpServlet
         }
         catch (ServletException ex)
         {
-            Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AccountController.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
         catch (IOException ex)
         {
-            Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AccountController.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
 
     }
 
     @Override
+    /**
+     * doGet receives things
+     * @param req request
+     * @param resp respones
+     */
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
@@ -60,21 +77,68 @@ public class AssignmentController extends HttpServlet
 
         view = req.getRequestDispatcher("/teacher/ManageAssignments.jsp");
 
-        req.setAttribute("teacherList", (ArrayList<Teacher>) Teacher.allTeachers());
-        req.setAttribute("teachCourseList", (ArrayList<Course>) (Teacher.allTeachers().get(0).getCourses()));
+        req.setAttribute("teacherList", (ArrayList<Teacher>) 
+                Teacher.allTeachers());
+        req.setAttribute("teachCourseList", (ArrayList<Course>) 
+                (Teacher.allTeachers().get(0).getCourses()));
         try
         {
             view.forward(req, resp);
         }
         catch (ServletException ex)
         {
-            Logger.getLogger(TeacherController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(TeacherController.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
         catch (IOException ex)
         {
-            Logger.getLogger(TeacherController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(TeacherController.class.getName()).
+                    log(Level.SEVERE, null, ex);
         }
 
     }
+    
+    private void modifyAssignment(String type, float max, float min, 
+            float average, Date dueDate, String name, Integer total, Assignment
+            current)
+    {
+        Assignment temp = current;
+        if(type != null)
+        {
+            temp.setType(type);
+        }
+        if(max >= 0)
+        {
+            temp.setMax(max);
+        }
+        if(min >= 0)
+        {
+            temp.setMin(min);
+        }
+        if(average >= 0)
+        {
+            temp.setAvg(average);
+        }
+        if(dueDate != null)
+        {
+            temp.setDueDate(dueDate);
+        }
+        if(name != null)
+        {
+            temp.setName(name);
+        }
+        if(total != null)
+        {
+            temp.setTotal(total);
+        }
+    }
+    
+    private void addAssignment(int id, String type, float max, float min, 
+            float average, Date dueDate, String name, Integer total)
+    {
+        Assignment temp = new Assignment(id);
+        modifyAssignment(type, max, min, average, dueDate, name, total, temp);
+    }
+    
 }
 
