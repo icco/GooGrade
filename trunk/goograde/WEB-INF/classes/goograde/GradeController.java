4b0e5f13f39711356136169d255663a6b863a94b
Katherine Blizard <kblizard@calpoly.edu>
2009-05-06 04:46:22 +0000

    Checkstyle GradingController

--- trunk/goograde/WEB-INF/classes/goograde/GradeController.java
+++ trunk/goograde/WEB-INF/classes/goograde/GradeController.java
@@ -15,53 +15,61 @@ import javax.servlet.http.HttpServletResponse;
  * Controls server interactions for grades. Tells what views to serve and gets 
  * data from the model, and related models.
  * @author nwelch
+ * @author kblizard
  */
 public class GradeController extends HttpServlet
 {
 
-    @Override
     /**
-     * doPost performs actions
-     * 
-     * @param req request
-     * @param resp response
+     * Fetches the information from a cookie and places it into an Account.
+     * @param user1 the Account being modified
+     * @param req the request ServeletRequest. 
+     * @return the accoutn modified. 
      */
-    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    private Account getCookie(Account user1, HttpServletRequest req)
     {
-        RequestDispatcher view = null;
-
-        Account user1 = new Account();
-        Integer courseId = new Integer((String) req.getAttribute("id"));
-        Course crse = new Course(courseId);
-
         // Gets the cookie we want
         for (Cookie cook : req.getCookies())
         {
+            /*Get the id of the account in order to load it */
             if (cook.getName().equals("userid"))
             {
                 user1.setId(new Integer(cook.getValue()));
                 user1.fetch();
             }
         }
-
-        // Are we a student or a teacher
-        if (user1.isTeacher() || user1.isTeacherAssistant())
-        {
-            view = req.getRequestDispatcher("/teacher/ManageGrades.jsp");
+        return user1;
+    }
+    
+    /**
+     * teacherSet fulfills the POST teacher request from doPost()
+     * @param req the request serveletRequest
+     * @param user1 the Account to use
+     * @param courseId teh ID of the course being modified. 
+     * @param crse the course being used. 
+     * @return the view
+     */
+    private RequestDispatcher teacherSet(HttpServletRequest req, Account user1,
+            Integer courseId, Course crse)
+    {
+        RequestDispatcher view =
+                req.getRequestDispatcher("/teacher/ManageGrades.jsp");
 
             Teacher user3;
             try
             {
                 user3 = new Teacher(user1.getId());
-                req.setAttribute("teachCourseList", (ArrayList<Course>) (user3.getCourses()));
+                req.setAttribute("teachCourseList",
+                        (ArrayList<Course>) (user3.getCourses()));
             }
             catch (Exception ex)
             {
-                Logger.getLogger(GradeController.class.getName()).log(Level.SEVERE, "Teacher Does Not Exist", ex);
+                Logger.getLogger(GradeController.class.getName())
+                        .log(Level.SEVERE, "Teacher Does Not Exist", ex);
             }
 
             Integer assId = new Integer(req.getParameter("ass"));
-            Integer stuId = new Integer(req.getParameter("stu"));
+            //Integer stuId = new Integer(req.getParameter("stu"));
 
             Assignment a1 = new Assignment(assId);
             Student s1 = new Student();
@@ -71,7 +79,33 @@ public class GradeController extends HttpServlet
             req.setAttribute("currentCourse", crse);
             req.setAttribute("id", courseId);
             req.setAttribute("assArray", crse.getAssignments());
-            req.setAttribute("stuArray", (ArrayList<Student>) crse.getStudents());
+            req.setAttribute("stuArray",
+                    (ArrayList<Student>) crse.getStudents());
+            return view;
+    }
+    
+    
+    /**
+     * doPost performs actions
+     * 
+     * @param req request
+     * @param resp response
+     */
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    {
+        RequestDispatcher view = null;
+
+        Account user1 = new Account();
+        Integer courseId = new Integer((String) req.getAttribute("id"));
+        Course crse = new Course(courseId);
+
+        user1 = getCookie(user1, req);
+
+        // Are we a student or a teacher
+        if (user1.isTeacher() || user1.isTeacherAssistant())
+        {
+            view = teacherSet(req, user1, courseId, crse);
         }
         else // Is a student
         {
@@ -80,17 +114,31 @@ public class GradeController extends HttpServlet
                 Student user2 = new Student(user1.getId());
                 view = req.getRequestDispatcher("/student/ViewGrades.jsp");
                 ArrayList<Grade> gradelist = user2.getGrades(crse);
-                req.setAttribute("enrolledCourseList", (ArrayList<Course>) (user2.getEnrolled()));
+                req.setAttribute("enrolledCourseList", 
+                        (ArrayList<Course>) (user2.getEnrolled()));
                 req.setAttribute("gradeList", gradelist);
                 req.setAttribute("currentCourse", crse);
                 req.setAttribute("id", courseId);
             }
             catch (Exception ex)
             {
-                Logger.getLogger(GradeController.class.getName()).log(Level.SEVERE, "Student Does Not Exist", ex);
+                Logger.getLogger(GradeController.class.getName())
+                        .log(Level.SEVERE, "Student Does Not Exist", ex);
             }
         }
 
+        viewForward(view, req, resp);
+    }
+
+    /**
+     * viewForward is a wrapper for view.forward with included try and catch
+     * @param view the RequestDispatcher for the view. 
+     * @param req the request
+     * @param resp teh response
+     */
+    private void viewForward(RequestDispatcher view, HttpServletRequest req, 
+            HttpServletResponse resp)
+    {
         try
         {
             view.forward(req, resp);
@@ -106,13 +154,12 @@ public class GradeController extends HttpServlet
                     log(Level.SEVERE, null, ex);
         }
     }
-
-    @Override
     /**
      * doGet receives things
      * @param req request
      * @param resp respones
      */
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
     {
         RequestDispatcher view = null;
@@ -121,15 +168,7 @@ public class GradeController extends HttpServlet
         Integer courseId = new Integer(req.getParameter("id"));
         Course crse = new Course(courseId);
 
-        // Gets the cookie we want
-        for (Cookie cook : req.getCookies())
-        {
-            if (cook.getName().equals("userid"))
-            {
-                user1.setId(new Integer(cook.getValue()));
-                user1.fetch();
-            }
-        }
+        user1 = getCookie(user1, req);
 
         // Are we a student or a teacher
         if (user1.isTeacher() || user1.isTeacherAssistant())
@@ -140,17 +179,20 @@ public class GradeController extends HttpServlet
             try
             {
                 user3 = new Teacher(user1.getId());
-                req.setAttribute("teachCourseList", (ArrayList<Course>) (user3.getCourses()));
+                req.setAttribute("teachCourseList",
+                        (ArrayList<Course>) (user3.getCourses()));
             }
             catch (Exception ex)
             {
-                Logger.getLogger(GradeController.class.getName()).log(Level.SEVERE, "Teacher Does Not Exist", ex);
+                Logger.getLogger(GradeController.class.getName())
+                        .log(Level.SEVERE, "Teacher Does Not Exist", ex);
             }
 
             req.setAttribute("currentCourse", crse);
             req.setAttribute("id", courseId);
             req.setAttribute("assArray", crse.getAssignments());
-            req.setAttribute("stuArray", (ArrayList<Student>) crse.getStudents());
+            req.setAttribute("stuArray",
+                    (ArrayList<Student>) crse.getStudents());
         }
         else // Is a student
         {
@@ -159,30 +201,19 @@ public class GradeController extends HttpServlet
                 Student user2 = new Student(user1.getId());
                 view = req.getRequestDispatcher("/student/ViewGrades.jsp");
                 ArrayList<Grade> gradelist = user2.getGrades(crse);
-                req.setAttribute("enrolledCourseList", (ArrayList<Course>) (user2.getEnrolled()));
+                req.setAttribute("enrolledCourseList",
+                        (ArrayList<Course>) (user2.getEnrolled()));
                 req.setAttribute("gradeList", gradelist);
                 req.setAttribute("currentCourse", crse);
                 req.setAttribute("id", courseId);
             }
             catch (Exception ex)
             {
-                Logger.getLogger(GradeController.class.getName()).log(Level.SEVERE, "Student Does Not Exist", ex);
+                Logger.getLogger(GradeController.class.getName())
+                        .log(Level.SEVERE, "Student Does Not Exist", ex);
             }
         }
 
-        try
-        {
-            view.forward(req, resp);
-        }
-        catch (ServletException ex)
-        {
-            Logger.getLogger(GradeController.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        }
-        catch (IOException ex)
-        {
-            Logger.getLogger(GradeController.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        }
+        viewForward(view, req, resp);
     }
 }
