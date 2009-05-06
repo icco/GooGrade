68909650a8661e6ee1cef9bbc2cb18558d6b7501
Nat Welch <nat@natwelch.com>
2009-05-06 05:07:18 +0000

    This will hopefully give us more accurate results for LOC.

--- trunk/statsvn.sh
+++ trunk/statsvn.sh
@@ -3,5 +3,5 @@
 cd /var/www/html/svn/trunk/; 
 rm -rvf /var/www/html/statsvn > /tmp/statsvn.log;
 svn log -v --xml > logfile.log;
-java -jar ../../statsvn.jar -trac http://wiki.csc.calpoly.edu/blugoo -output-dir /var/www/html/statsvn logfile.log . &> /tmp/statsvn.log
+java -jar ../../statsvn.jar -trac http://wiki.csc.calpoly.edu/blugoo -output-dir /var/www/html/statsvn logfile.log -exclude "goograde/lib" -exclude "goograde/WEB-INF/web/styles". &> /tmp/statsvn.log
 
