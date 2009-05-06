85652f32d63c300a51ac5b88847e75b9a613e5f3
Nat Welch <nat@natwelch.com>
2009-05-06 07:34:20 +0000

    I feel silly doing all of these little commits but this fixes the issue where our build script has too much junk in it.

--- trunk/statsvn.sh
+++ trunk/statsvn.sh
@@ -3,5 +3,5 @@
 cd /var/www/html/svn/trunk/; 
 rm -rvf /var/www/html/statsvn > /tmp/statsvn.log;
 svn log -v --xml > logfile.log;
-java -jar ../../statsvn.jar -no-developer bot -output-dir /var/www/html/statsvn -exclude "goograde/nbproject/;goograde/lib/;goograde/WEB-INF/web/styles/" logfile.log . 
+java -jar ../../statsvn.jar -no-developer bot -output-dir /var/www/html/statsvn -exclude "goograde/nbproject/;goograde/lib/;goograde/WEB-INF/web/styles/" logfile.log . &> /tmp/statsvn.log
 
