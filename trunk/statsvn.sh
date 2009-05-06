9448a41d630958d5780ae2b5c9186b3e6a30f9f1
Nat Welch <nat@natwelch.com>
2009-05-06 05:30:05 +0000

    Alright, last change now. Ignores libraries, but doesn't seperate out language types. I have another script that will do that. I think I'll keep that out because I just need it at release.

--- trunk/statsvn.sh
+++ trunk/statsvn.sh
@@ -3,5 +3,5 @@
 cd /var/www/html/svn/trunk/; 
 rm -rvf /var/www/html/statsvn > /tmp/statsvn.log;
 svn log -v --xml > logfile.log;
-java -jar ../../statsvn.jar -trac http://wiki.csc.calpoly.edu/blugoo -output-dir /var/www/html/statsvn logfile.log -exclude "goograde/lib" -exclude "goograde/WEB-INF/web/styles". &> /tmp/statsvn.log
+java -jar ../../statsvn.jar -trac http://wiki.csc.calpoly.edu/blugoo -no-developer bot -output-dir /var/www/html/statsvn -exclude "goograde/nbproject/;goograde/lib/;goograde/WEB-INF/web/styles/" logfile.log . 
 
