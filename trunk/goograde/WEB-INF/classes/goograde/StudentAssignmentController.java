a870ac18929c43e4725ec63da47f9b97abe330f3
Katherine Blizard <kblizard@calpoly.edu>
2009-05-06 03:54:22 +0000

    Checkstyle

--- trunk/goograde/WEB-INF/classes/goograde/StudentAssignmentController.java
+++ trunk/goograde/WEB-INF/classes/goograde/StudentAssignmentController.java
@@ -1,8 +1,6 @@
 package goograde;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 import javax.servlet.http.Cookie;
 
 /**
@@ -54,8 +51,10 @@ public class StudentAssignmentController extends HttpServlet
         Integer courseId = new Integer(req.getParameter("id"));
         Course crse = new Course(courseId);
 
+        /*Check cookies to ger a student */
         for (Cookie cook : req.getCookies())
         {
+            /*Fetching Id needed, then creating the Student */
             if (cook.getName().equals("userid"))
             {
                 try
@@ -64,7 +63,8 @@ public class StudentAssignmentController extends HttpServlet
                 }
                 catch (Exception ex)
                 {
-                    Logger.getLogger(StudentAssignmentController.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(StudentAssignmentController.class.
+                            getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
@@ -73,7 +73,8 @@ public class StudentAssignmentController extends HttpServlet
         req.setAttribute("id", (String) req.getParameter("id"));
         req.setAttribute("enrolledCourseList", user1.getEnrolled());
         req.setAttribute("currentCourse", crse);
-        req.setAttribute("AssignmentList", (ArrayList<Assignment>) crse.getAssignments());
+        req.setAttribute("AssignmentList", (ArrayList<Assignment>)
+                crse.getAssignments());
 
         /*req.setAttribute("AssignmentList", (ArrayList<Course>) 
         (Teacher.allTeachers().get(0).getCourses())); */
