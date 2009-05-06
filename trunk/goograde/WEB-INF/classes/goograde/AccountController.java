fc7b0923ab6714c9c7685dbd6d111e8f2918d5e6
Viktor Gerdin <vgerdin@calpoly.edu>
2009-05-06 04:42:20 +0000

    Stylish AccountController as in #322

--- trunk/goograde/WEB-INF/classes/goograde/AccountController.java
+++ trunk/goograde/WEB-INF/classes/goograde/AccountController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServlet;
  * the Account controller. Not really sure what it should be 
  * doing, but we'll get to that.
  * 
- * @author bluGoo
+ * @author bluGoo, vgerdin
  * @version 0.42
  */
 public class AccountController extends HttpServlet
@@ -30,85 +30,13 @@ public class AccountController extends HttpServlet
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
     {
-        Course thiscourse = new Course(new Integer(req.getParameter("id")));
-        String action = req.getParameter("action");
-        String type = req.getParameter("type");
 
+        String action = req.getParameter("action");
+
+        //We do not like null pointer exceptions
         if (action != null)
         {
-            //Do something depending on the hidden field
-            if (action.equals("delete"))
-            {
-                try
-                {
-                    thiscourse.removeStudent(null, new Student(new Integer(req.getParameter("accountRef"))));
-                }
-                catch (Exception ex)
-                {
-                    Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, "Error Unenrolling a student.", ex);
-                }
-            }
-            else if (action.equals("add"))
-            {
-                Account toAdd = new Account(req.getParameter("newUserName"),
-                        req.getParameter("newFullName"),
-                        req.getParameter("newEmailAddr"));
-                toAdd.save();
-                toAdd.fetch();
-                if(type != null)
-                {
-                    if(type.equals("student"))
-                    {
-                        thiscourse.addStudent(null, toAdd);
-                    }
-                    else if(type.equals("teacherAssistant"))
-                    {
-                        thiscourse.addTA(null, toAdd);
-                    }
-                }
-                
-            }
-            else if (action.equals("edit"))
-            {
-                Account tmp = null;
-                try
-                {
-                    tmp = new Account(new Integer(req.getParameter("accountRef")));
-                }
-                catch (Exception ex)
-                {
-                    Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                tmp.setUserName(req.getParameter("newUserName"));
-                tmp.setEmailAddress(new EmailAddress(req.getParameter("newEmailAddr")));
-                tmp.setFullName(req.getParameter("newFullName"));
-                
-                tmp.save();
-            }
-            else if (action.equals("addOld"))
-            {
-                Account toAdd = null;
-                try
-                {
-                    toAdd = new Account(new Integer(req.getParameter("accountRef")));
-                }
-                catch (Exception ex)
-                {
-                    Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                if(type != null)
-                {
-                    if(type.equals("student"))
-                    {
-                        thiscourse.addStudent(null, toAdd);
-                    }
-                    else if(type.equals("teacherAssistant"))
-                    {
-                        thiscourse.addTA(null, toAdd);
-                    }
-                }
-            }
+            this.doAction(req, action);
         }
 
         this.doGet(req, resp);
@@ -126,7 +54,7 @@ public class AccountController extends HttpServlet
         RequestDispatcher view = req.getRequestDispatcher("/teacher/ManageAccounts.jsp");
         req.setAttribute("teachCourseList",
                 (ArrayList<Course>) (Teacher.allTeachers().get(0).getCourses()));
-        
+
         req.setAttribute("allAccountList", Student.allAccounts());
 
         Course thiscourse = new Course(new Integer(req.getParameter("id")));
@@ -136,11 +64,11 @@ public class AccountController extends HttpServlet
 
         members.addAll(thiscourse.getTeacherAssistants());
         members.addAll(thiscourse.getStudents());
-        
+
 
         req.setAttribute("accountList", members);
         req.setAttribute("tas", thiscourse.getTeacherAssistants());
-        
+
         req.setAttribute("currentCourse", thiscourse);
 
         try
@@ -149,11 +77,15 @@ public class AccountController extends HttpServlet
         }
         catch (ServletException ex)
         {
-            Logger.getLogger(TeacherController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(
+                    TeacherController.class.getName()).log(
+                    Level.SEVERE, null, ex);
         }
         catch (IOException ex)
         {
-            Logger.getLogger(TeacherController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(
+                    TeacherController.class.getName()).log(
+                    Level.SEVERE, null, ex);
         }
     }
 
@@ -172,8 +104,6 @@ public class AccountController extends HttpServlet
         temp.setUserName(uName);
         temp.setFullName(fName);
         temp.save();
-        
-        
     }
 
     /**
@@ -190,9 +120,140 @@ public class AccountController extends HttpServlet
         }
         catch (Exception ex)
         {
-            Logger.getLogger(AccountController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(
+                    AccountController.class.getName()).log(
+                    Level.SEVERE, null, ex);
         }
         temp.delete();
+    }
+
+    private boolean doAction(HttpServletRequest req, String action)
+    {
+        boolean ret = false;
+        
+        //Do something depending on the hidden field
+        //We want to delete
+        if (action.equals("delete"))
+        {
+            ret = doActionDelete(req);
+        }
+        //We want to add a new
+        else if (action.equals("add"))
+        {
+            ret = doActionAdd(req);
+        }
+        //We want to edit
+        else if (action.equals("edit"))
+        {
+            ret = doActionEdit(req);
+        }
+        //We want to add an old
+        else if (action.equals("addOld"))
+        {
+            ret = doActionAddOld(req);
+            
+        }
+        return ret;
+    }
+
+    private boolean doActionDelete(HttpServletRequest req)
+    {
+        boolean ret = false;
+        Course thiscourse = new Course(new Integer(req.getParameter("id")));
+        try
+        {
+            ret = thiscourse.removeStudent(null, 
+                    new Student(new Integer(req.getParameter("accountRef"))));
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(
+                    AccountController.class.getName()).log(
+                    Level.SEVERE, "Error Unenrolling a student.", ex);
+        }
+        return ret;
+    }
+    
+    private boolean doActionAdd(HttpServletRequest req)
+    {
+        boolean ret = false;
+        Course thiscourse = new Course(new Integer(req.getParameter("id")));
+        String type = req.getParameter("type");
+        Account toAdd = new Account(req.getParameter("newUserName"),
+                        req.getParameter("newFullName"),
+                        req.getParameter("newEmailAddr"));
+        toAdd.save();
+        toAdd.fetch();
+        //We do not like null pointer exceptions
+        if (type != null)
+        {
+            //We want to add this one as a student
+            if (type.equals("student"))
+            {
+                ret = thiscourse.addStudent(null, toAdd);
+            }
+            //We want to add this one as a ta
+            else if (type.equals("teacherAssistant"))
+            {
+                ret = thiscourse.addTA(null, toAdd);
+            }
+        }
+        return ret;
+    }
+
+    private boolean doActionEdit(HttpServletRequest req)
+    {
+        boolean ret = false;
+        Account tmp = null;
+        try
+        {
+            tmp = new Account(new Integer(req.getParameter("accountRef")));
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(
+                    AccountController.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+        tmp.setUserName(req.getParameter("newUserName"));
+        tmp.setEmailAddress(new EmailAddress(req.getParameter("newEmailAddr")));
+        tmp.setFullName(req.getParameter("newFullName"));
+
+        ret = tmp.save();
+        return ret;
+    }
+    
+    private boolean doActionAddOld(HttpServletRequest req)
+    {
+        boolean ret = false;
+        Course thiscourse = new Course(new Integer(req.getParameter("id")));
+        String type = req.getParameter("type");
+        Account toAdd = null;
+        try
+        {
+            toAdd = new Account(new Integer(req.getParameter("accountRef")));
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(
+                    AccountController.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+        //We do not like null pointer exceptions
+        if (type != null)
+        {
+            //We want to add this one as a studet
+            if (type.equals("student"))
+            {
+                ret = thiscourse.addStudent(null, toAdd);
+            }
+            //We want to add this one as a ta
+            else if (type.equals("teacherAssistant"))
+            {
+                ret = thiscourse.addTA(null, toAdd);
+            }
+        }
+        return ret;
     }
 }
 
