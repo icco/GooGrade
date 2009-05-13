76cdfea5fefe1be62293b5d36eec5a1e073a8ec3
Nat Welch <nat@natwelch.com>
2009-05-13 03:42:23 +0000

    A class we might need.

---
+++ trunk/goograde/WEB-INF/classes/goograde/Metrics.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goograde;
+
+/**
+ * http://code.google.com/apis/chart/types.html
+ * http://code.google.com/p/flot/
+ */
+public final class Metrics
+{
+}
