20938f4cd6b3da781e4a5ca39447e2c160137dbe
Nat Welch <nat@natwelch.com>
2009-05-17 23:29:20 +0000

    Working on getting emma into our test builds. I couldn't get ant working, but at least emma should be scripted.

--- trunk/testbuild.sh
+++ trunk/testbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A daily Build Script for blugoo
+# A daily test script for blugoo
 
 # Variables
 SUBJECT="Daily Test Report for `date +%D`"
@@ -7,7 +7,14 @@ EMAIL="blugoo@lists.csc.calpoly.edu"
 MESSAGE_FILE="/var/www/html/test/test.`date +%F`.log"
 
 cd /var/www/html/svn/trunk/goograde/; 
+# ant &> /dev/null # This should be enabled if not being run after the daily build script
+java -cp lib/emma.jar:. emma instr -ip . -m overwrite
 ant test &> $MESSAGE_FILE
+java -cp lib/emma.jar emma report -r html -sp . -in coverage.em -in coverage.ec
+
+# cp up.
+rm -rf /var/www/html/coverage/
+cp -r ./coverage/ /var/www/html/
 
 # Mail it!!!
 /bin/mail -s "$SUBJECT" "$EMAIL" < $MESSAGE_FILE
