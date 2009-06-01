4e0481df570ee4278b1d10f01a29c5a1e8e42bdd
Nat Welch <nat@natwelch.com>
2009-06-01 00:24:58 +0000

    An attempt to get statsvn right.

--- trunk/statsvn.sh
+++ trunk/statsvn.sh
@@ -2,6 +2,6 @@
 
 cd /var/www/html/svn/trunk/; 
 rm -rvf /var/www/html/statsvn > /tmp/statsvn.log;
-svn log -v --xml > logfile.log;
+svn log -v --xml -r 815:HEAD> logfile.log;
 java -jar ../../statsvn.jar -no-developer bot -output-dir /var/www/html/statsvn -exclude "goograde/nbproject/;goograde/lib/;goograde/WEB-INF/web/styles/" logfile.log . &> /tmp/statsvn.log
 
